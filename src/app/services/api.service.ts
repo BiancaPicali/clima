6bdfb0b350b149b6ca9248c8791e1772b0e39bbd
Ingrid Heloise <61015082+ingridfreitas@users.noreply.github.com>
2022-09-29 11:24:52 -0300

    Clima

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  private url = 'https://reqres.in/api/users'
+
+  constructor(private http: HttpClient) { }
+
+  buscarUsuario(page: number){
+    return this.http.get(`${this.url}?page=${page}`)
+  }
+}
