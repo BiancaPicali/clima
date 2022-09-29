0852f015321abd5fd64b0b9d5903cc48cb261389
biancapicali <bianca.picali@etec.sp.gov.br>
2022-09-29 12:16:22 -0300

    perdicopy
    perdi meu projeto, peguei o da Ingrid para não ficar atrasada, logo após dei continuidade com as variaveis do console

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,12 +7,24 @@ import { ApiService } from '../services/api.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  page;
+  perPage;
+  total;
+  totalPages;
+  data;
 
-  constructor(private api: ApiService) {}
+  constructor(private api: ApiService) {
+    this.buscarTodos()
+  }
 
   buscarTodos(page = 1){
     this.api.buscarUsuario(page).subscribe(usuarios => {
-      
+      console.log(usuarios);
+      this.page = usuarios['page'];
+      this.perPage = usuarios['per_page'];
+      this.total = usuarios['total'];
+      this.totalPages = usuarios['total_pages'];
+      this.data = usuarios['data'];
     });
   }
 
